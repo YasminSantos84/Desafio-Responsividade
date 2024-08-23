d5ad17fa6c0cd874e8a7ca0076f529142c78cf77
YasminSantos84 <79729170+YasminSantos84@users.noreply.github.com>
2024-08-23 11:41:44 -0300

    initial v2

---
+++ Desafio_Responsividade/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,9 @@
+import styles from "./avatar.module.css"
+
+export const Avatar = () => {
+    return (
+        <div className={styles.avatarPhoto}>
+            Avatar aqui
+        </div>
+    )
+}
---
+++ Desafio_Responsividade/src/components/Canal/Canal.jsx
@@ -0,0 +1,12 @@
+import Avatar from "../Avatar/Avatar.jsx"
+import Box from '@material-ui/core/Box';
+import Styles from "./Canal.module.css"
+
+export const Canal = () => {
+    return (
+        <Box classname={Styles.caixaCanal}>
+            <Avatar />
+        </Box>
+    )
+
+}
