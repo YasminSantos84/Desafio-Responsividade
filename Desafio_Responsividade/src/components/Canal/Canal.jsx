3f7b70221297e7e853c6926dd162a53bf0539066
YasminSantos84 <yasminap84@outlook.com>
2024-09-01 23:56:55 -0300

    ajuste de faixa de canal

--- Desafio_Responsividade/src/components/Canal/Canal.jsx
+++ Desafio_Responsividade/src/components/Canal/Canal.jsx
@@ -1,40 +1,33 @@
-import {Avatar, Box, colors} from "@mui/material";
-import "./canal.module.css"
-import { Grid } from "@mui/material";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button"
+import { Avatar, Box, Grid, Stack, Button } from "@mui/material";
 import { red } from "@mui/material/colors";
-import {styled} from "@mui/material/styles";
- 
-const ColorButtonRed = styled(Button)(({theme}) => ({
-    color: theme.pallete.getContrastText(red[400]),
-    backgroundColor:red[400],
-    '&:hover':{
-        backgroundColor: red[600],
-    },
+import { styled } from "@mui/material/styles";
+import styles from "./canal.module.css"; // Importar CSS module como objeto
+
+const ColorButtonRed = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(red[400]),
+  backgroundColor: red[400],
+  "&:hover": {
+    backgroundColor: red[600],
+  },
 }));
 
 export const Canal = () => {
-    return (
-        <Box 
-            className = {'caixaCanal'}
-            sx={{ flexGrow:1, overflow:'hidden', px:3}}>           
-            <Grid 
-                container wrap="nowrap" 
-                spacing={2}>
-                <Grid item>
-                    <Avatar>Y</Avatar>
-                </Grid>
-                <Grid item xs zeroMinWidth>
-                    <p >Canal Exemplo Desafio</p>
-                    <p className='nSubscribes'>10 inscritos</p>
-                </Grid>
-                <Grid item>
-                    <Stack>
-                        <ColorButtonRed  variant="contained">Subscribe</ColorButtonRed >
-                    </Stack>                    
-                </Grid>
-            </Grid>
-        </Box>
-    )
-}
+  return (
+    <Box className={styles.caixaCanal} sx={{ flexGrow: 1, overflow: "hidden", px: 3 }}>
+      <Grid container wrap="nowrap" spacing={2}>
+        <Grid item>
+          <Avatar>Y</Avatar>
+        </Grid>
+        <Grid item xs zeroMinWidth>
+          <p>Canal Exemplo Desafio</p>
+          <p className={styles.nSubscribes}>10 inscritos</p>
+        </Grid>
+        <Grid item>
+          <Stack>
+            <ColorButtonRed variant="contained">Subscribe</ColorButtonRed>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
