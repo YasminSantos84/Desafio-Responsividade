83e0196f163dbff602a74fa2438ae021fc22321c
YasminSantos84 <yasminap84@outlook.com>
2024-09-17 21:48:58 -0300

    Ajustes grid

--- Desafio_Responsividade/src/App.jsx
+++ Desafio_Responsividade/src/App.jsx
@@ -3,15 +3,35 @@ import './App.css'
 import { Canal } from './components/Canal/Canal'
 import { Comentario } from './components/Comentarios/Comentario'
 import { BoxVideo } from './components/Exibicao/BoxVideo/BoxVideo'
+import { Preview } from './components/Recomendacoes/Preview/Preview'
+import Grid from '@mui/material/Unstable_Grid2/Grid2'
+import { Box } from '@mui/material';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <BoxVideo/>   
-      <Canal/>
-      <Comentario/>      
+      <Box sx={{ flexGrow: 1, p: 2 }}>
+      <Grid
+        container
+        sx={{
+          '--Grid-borderWidth': '1px',
+          borderTop: 'var(--Grid-borderWidth) solid',
+          borderLeft: 'var(--Grid-borderWidth) solid',
+          borderColor: 'divider',
+          '& > div': {
+            borderRight: 'var(--Grid-borderWidth) solid',
+            borderBottom: 'var(--Grid-borderWidth) solid',
+            borderColor: 'divider',
+          },
+        }}>
+          <BoxVideo/>   
+          <Canal/>
+          <Comentario/> 
+          <Preview/>              
+        </Grid>
+      </Box> 
     </>
   )
 }
